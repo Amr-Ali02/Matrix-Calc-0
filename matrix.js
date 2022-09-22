d4579e118f6f94407c7f257c599e02c2aac9b190
Amr <amrmedhat01024387747@.com>
2022-09-22 12:43:24 +0200

    matset on

--- matrix.js
+++ matrix.js
@@ -1,10 +1,21 @@
-let mat1 = document.querySelector("mat1");
 let rowa = document.querySelector("#rowa");
 let cola = document.querySelector("#cola");
-rowa.addEventListener("input", () => {
+let s1 = document.querySelector(".s1");
+rowa.addEventListener("input", set1);
+cola.addEventListener("input", set1);
+function set1() {
+  s1.style.cssText = `
+display: grid;
+grid-template-columns: repeat(${cola.value},1fr);
+grid-template-rows: repeat(${rowa.value},1fr);
+`;
+s1.innerHTML = "";
   for (let i = 1; i <= rowa.value; i++) {
     for (let j = 1; j <= cola.value; j++) {
-      console.log(i, j);
+      let mye = document.createElement("input");
+      mye.setAttribute("type", "number");
+      mye.id = `a${i}${j}`;
+      s1.appendChild(mye);
     }
   }
-});
+}
